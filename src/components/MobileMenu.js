77f34c712fc1ed449b019ef2876be82c74adb801
Konstantin <altwebga@yandex.ru>
2022-08-16 17:09:18 +0700

    RP12 mobile menu

---
+++ src/components/MobileMenu.js
@@ -0,0 +1,16 @@
+const MobileMenu = ({ email, signOut, isMobileMenuOpen }) => {
+  return (
+    <div className={`mobile-menu ${isMobileMenuOpen && "mobile-menu_opened"}`}>
+      <h2 className="mobile-menu__email">{email}</h2>
+      <button
+        className="mobile-menu__exit"
+        type="button"
+        onClick={signOut}
+      >
+        Выйти
+      </button>
+    </div>
+  );
+};
+
+export default MobileMenu;
