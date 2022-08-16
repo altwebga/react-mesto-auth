352c520bf569cf33d84b8c6088422fa0231fdeda
Konstantin <altwebga@yandex.ru>
2022-08-16 17:16:56 +0700

    PR12 mobile menu fix

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -1,7 +1,7 @@
 const MobileMenu = ({ email, signOut, isMobileMenuOpen }) => {
   return (
     <div className={`mobile-menu ${isMobileMenuOpen && "mobile-menu_opened"}`}>
-      <h2 className="mobile-menu__email">{email}</h2>
+      <h2 className="mobile-menu__user-area">{email}</h2>
       <button
         className="mobile-menu__exit"
         type="button"
